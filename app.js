95644f14fbe5e64ed0fcbbbb674affe8f442585e
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-12 01:14:15 +1200

    comit

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ const taskinput = $("#task")
 loadEventListeners()
 
 function loadEventListeners() {
-    form.addEventListener("submit", addTask)
+    $(document).on("submit","#tasks-form", addTask)
 }
 
-function addTask() {
-    if(taskinput.value==="") {
+function addTask(e) {
+
+    if(taskinput.val()==="") {
         alert("")
     }
 
-    const li = $("li.collection-item")
+    const li = $("li .collection-item")
+    $("<div>").val(taskinput.val()).appendTo(li)
     
+    const link =$("a .delete-item .secondary-content").html("<i class='fa fa-remove'</i>")
+    link.appendTo(li)
+    tasklist.append(li)
+    taskinput.val("")
+    console.log(li)
+    e.preventDefault()
+
 }
