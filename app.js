8b71da9a2e1a5371a158a516f8e165b2c0d53878
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-12 01:22:48 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const form = $("#tasks-form")
+const form = $("#task-form")
 const tasklist = $(".collection")
 const clearBtn = $("#clear-tasks")
 const taskinput = $("#task")
 
 
-
-loadEventListeners()
-
-function loadEventListeners() {
-    $(document).on("submit","#tasks-form", addTask)
-}
-
-function addTask(e) {
-
-    if(taskinput.val()==="") {
-        alert("")
-    }
-
-    const li = $("li .collection-item")
-    $("<div>").val(taskinput.val()).appendTo(li)
+$(document).on("submit","#task-form", function (e){
     
-    const link =$("a .delete-item .secondary-content").html("<i class='fa fa-remove'</i>")
-    link.appendTo(li)
-    tasklist.append(li)
-    taskinput.val("")
-    console.log(li)
-    e.preventDefault()
 
-}
+        if(taskinput.val()==="") {
+            alert("")
+        }
+    
+        const li = $("<li class='collection-item'>")
+        console.log(li)
+        var string = $("<div>").html(taskinput.val())
+        console.log(string)
+        string.appendTo(li)
+        
+        const link =$("<a class=delete-item secondary-content>").html("<i class='fa fa-remove'</i>")
+        console.log(li)
+        link.appendTo(li)
+        tasklist.append(li)
+        console.log(li)
+        taskinput.val("")
+        console.log(li)
+        e.preventDefault()
+    
+    
+})
