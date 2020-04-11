b4c1ad4747a1f6ab777be955fd9e00fdf5030dcc
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-12 03:01:49 +1200

    commit

--- app.js
+++ app.js
@@ -1,30 +1,67 @@
 const form = $("#task-form")
 const tasklist = $(".collection")
-const clearBtn = $("#clear-tasks")
+const clearBtn = $(".clear-tasks")
 const taskinput = $("#task")
 
 
-$(document).on("submit","#task-form", function (e){
-    
+const filter = $("#filter")
+clearBtn.click(clearTasks)
 
-        if(taskinput.val()==="") {
-            alert("")
-        }
-    
+tasklist.click(removeTask)
+
+$(document).on("submit", "#task-form", function (e) {
+
+
+    if (taskinput.val() === "") {
+        alert("add a task please")
+    } else {
+
         const li = $("<li class='collection-item'>")
-        console.log(li)
-        var string = $("<div>").html(taskinput.val())
-        console.log(string)
-        string.appendTo(li)
-        
-        const link =$("<a class=delete-item secondary-content>").html("<i class='fa fa-remove'</i>")
-        console.log(li)
+
+
+        li.html(taskinput.val())
+
+        const link = $("<a class='delete-item secondary-content'>").html("<i class='fa fa-remove'></i>")
+
         link.appendTo(li)
         tasklist.append(li)
-        console.log(li)
+
         taskinput.val("")
-        console.log(li)
+
         e.preventDefault()
-    
-    
+
+    }
+
+
 })
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm("are you sure")) e.target.parentElement.parentElement.remove()
+    }
+}
+
+function clearTasks(e) {
+    tasklist.html("")
+}
+
+
+filter.on('keyup', function (e) {
+    const keyword = filter.val().toLowerCase();
+    var tasks = $(".collection-item")
+    tasks.toArray().forEach((task) => {
+        
+        try {
+            $(task).children().each(() => {
+                if (this.val() === keyword) {
+                    tasks.hide();
+                    task.show();
+                }
+            })
+        } catch (e) {
+            
+        }
+
+    })
+})
+
